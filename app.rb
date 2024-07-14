2041a5a20a3b1095e68da867b1bfadc9a949815f
ritorocks <ritorocks@gmail.com>
2024-07-14 01:33:04 +0000

    Added get modules to app.rb & added top navigation to layout.erb

--- app.rb
+++ app.rb
@@ -7,3 +7,15 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get ("/rock") do
+  "We played rock!"
+end
+
+get ("/paper") do
+  "We played paper!"
+end
+
+get ("/scissors") do
+  "We played scissors!"
+end
